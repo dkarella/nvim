0fa283be07951f4085b63d58228616828133fa34
Danny Arellano <dkarella@DannyPC>
2023-11-14 23:00:54 -0800

    ignorecase + smartcase

--- lua/dkarella/init.lua
+++ lua/dkarella/init.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.opt.swapfile = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 require("dkarella.lazy")
 require("dkarella.remap")
